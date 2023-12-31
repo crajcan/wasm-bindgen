a6e8bfafa2049f78f5d7db3264d31decc28eca4e
Liamolucko <liampm32@gmail.com>
2023-12-31 03:28:03 +0000

    Deploying to gh-pages from @ rustwasm/wasm-bindgen@a3332e4be6ae3339dd77f37257e3d4d170085099 🚀

---
+++ exbuild/websockets/pkg/websockets_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start_websocket(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke0_mut__hfa55077075101bd6(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut__h16b00b2244bc2e00(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
